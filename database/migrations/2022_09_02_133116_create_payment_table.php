e188fbfedd3da605d0aa787b3c7883363cc74a17
kingsley achumie <kingsonly13c@gmail.com>
2022-09-02 14:40:56 +0100

    wip

---
+++ database/migrations/2022_09_02_133116_create_payment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->string('amount');
+            $table->string('actual_amount');
+            $table->string('type_of_payment');
+            //$table->string('mode_of_payment');
+            $table->integer('order_id');
+            $table->integer('project_id');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
